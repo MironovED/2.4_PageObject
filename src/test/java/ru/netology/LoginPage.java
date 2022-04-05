10d3474725953f148dd51c593544861d521e8910
Evgeny Mironov <e.d.mironov@yandex.ru>
2022-04-05 19:37:43 +0300

    preliminary commit

---
+++ src/test/java/ru/netology/LoginPage.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage {
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        $("[data-test-id='login'] .input__control").val(info.getLogin());
+        $("[data-test-id='password'] .input__control").val(info.getPassword());
+        $(".button").click();
+        return new VerificationPage();
+    }
+
+}
---
+++ src/test/java/ru/netology/VerificationPage.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class VerificationPage {
+    private SelenideElement codeField = $("[data-test-id='code'] .input__control");
+    private SelenideElement verifyButton = $(".button");
+
+    public VerificationPage() {
+        codeField.shouldBe(visible);
+    }
+
+    public DashboardPage validVerify(DataHelper.VerificationCode verificationCode) {
+        codeField.val(verificationCode.getCode());
+        verifyButton.click();
+        return new DashboardPage();
+    }
+
+}
