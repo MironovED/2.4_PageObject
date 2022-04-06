1fed0666919548df4d9dd723de2cb926a0833d8b
Evgeny Mironov <e.d.mironov@yandex.ru>
2022-04-06 23:23:24 +0300

    preliminary commit

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -4,8 +4,11 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import lombok.val;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
@@ -14,9 +17,9 @@ public class DashboardPage {
     private ElementsCollection cards = $$(".list__item div");
     private final String balanceStart = "баланс: ";
     private final String balanceFinish = " р.";
-    private ElementsCollection replenishCard = $$("[data-test-id='action-deposit']");
-    private SelenideElement amount = $("[data-test-id='amount']");
-    private SelenideElement fromCard = $("[data-test-id='from']");
+//    private ElementsCollection replenishCard = $$("[data-test-id='action-deposit'] .button__content");
+    private SelenideElement amount = $("[data-test-id='amount'] .input__control");
+    private SelenideElement fromCard = $("[data-test-id='from'] .input__control");
     private SelenideElement topUpButton = $("[data-test-id='action-transfer']");
 
 
@@ -25,7 +28,7 @@ public class DashboardPage {
     }
 
     public int getCardBalance(String id) {
-        val text = cards.findBy(text(id)).text();
+        val text = cards.findBy(id(id)).text();
         return extractBalance(text);
     }
 
@@ -36,10 +39,12 @@ public class DashboardPage {
         return Integer.parseInt(value);
     }
 
-    public DashboardPage transferMoney(String inCard, String value, String outCard) {
-        replenishCard.findBy(text(inCard)).click();
+    public DashboardPage transferMoney(String inCard, String value, String numberCard) {
+        $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] [data-test-id='action-deposit']").click();
+//        $(By.id(inCard)).find("[data-test-id='action-deposit']").click();
+//        cards.findBy(id(inCard)).find("[.button__text]").click();
         amount.setValue(value);
-        fromCard.setValue(outCard);
+        fromCard.setValue(numberCard);
         topUpButton.click();
         return new DashboardPage();
     }
--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -1,14 +1,43 @@
 package ru.netology.test;
 
+
+
 import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPage;
+
+import java.util.Arrays;
 
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class MoneyTransferTest {
+    private String firstCard = "92df3f1c-a033-48e6-8390-206f6b1f56c0";
+    private String secondCard = "0f3f5c2a-249e-4c3d-8287-09f7a039391d";
+    private String firstNumberCard = "5559_0000_0000_0001";
+    private String secondNumberCard = "5559_0000_0000_0001";
+
 
     @Test
     public void should () {
         open("http://0.0.0.0:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+        var dashboardPage = new DashboardPage();
+        dashboardPage.transferMoney(firstCard,"1000",secondNumberCard);
+
+        dashboardPage.getCardBalance(firstCard);
+
+
+
+
+
+
+
 
     }
 
